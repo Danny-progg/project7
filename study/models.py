b195f3cea2b6d89cdaa5636171cde3fafa7e5c11
Danny-progg <122675148+Danny-progg@users.noreply.github.com>
2023-11-03 20:52:13 +0300

    models for the lesson are described and a CRUD mechanism is created

--- study/models.py
+++ study/models.py
@@ -5,8 +5,8 @@ NULLABLE = {'blank': True, 'null': True}
 
 class Course(models.Model):
     title = models.CharField(max_length=150, verbose_name='название')
-    description = models.TextField(verbose_name='название')
-    preview = models.ImageField(upload_to='images/', verbose_name='Изображение', **NULLABLE)
+    description = models.TextField(verbose_name='описание')
+    preview = models.ImageField(upload_to='images/', verbose_name='изображение', **NULLABLE)
 
     def __str__(self):
         return f'{self.title}'
@@ -14,3 +14,17 @@ class Course(models.Model):
     class Meta:
         verbose_name = 'курс'
         verbose_name_plural = 'курсы'
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    preview = models.ImageField(upload_to='images/', verbose_name='изображение', **NULLABLE)
+    link_video = models.CharField(max_length=150, verbose_name='ссылка на видео', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
--- study/urls.py
+++ study/urls.py
@@ -1,7 +1,10 @@
+from django.urls import path
+
 from study.apps import StudyConfig
 from rest_framework.routers import DefaultRouter
 
-from study.views import CourseViewSet
+from study.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
+    LessonUpdateAPIView, LessonDestroyAPIView
 
 app_name = StudyConfig.name
 
@@ -9,5 +12,9 @@ router = DefaultRouter()
 router.register(r'courses', CourseViewSet, basename='courses')
 
 urlpatterns = [
-
+    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
+    path('lesson/', LessonListAPIView.as_view(), name='lesson-list'),
+    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-get'),
+    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
+    path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-delete')
 ] + router.urls
