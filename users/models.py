fa542a544fd279ea38d7d7e7d99f40d04e7e4941
Danny-progg <122675148+Danny-progg@users.noreply.github.com>
2023-11-03 20:04:38 +0300

    models for the user are described

--- users/models.py
+++ users/models.py
@@ -1,5 +1,17 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
 
 
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+
+    phone = models.CharField(max_length=25, verbose_name='телефон', **NULLABLE)
+    country = models.CharField(max_length=50, verbose_name='страна', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    vrf_token = models.CharField(max_length=25, verbose_name='токен', **NULLABLE)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
