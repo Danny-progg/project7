229569681ae60b8cbf0296e3ed8c8eba01fc48ac
Danny-progg <122675148+Danny-progg@users.noreply.github.com>
2023-11-08 20:07:12 +0300

    fixed adding a field for displaying the number of lessons

--- study/serliazers.py
+++ study/serliazers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from study.models import Course, Lesson
 
@@ -6,10 +7,15 @@ from study.models import Course, Lesson
 class CourseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = ("id", "title", "description")
 
 
 class LessonSerializer(serializers.ModelSerializer):
+    lessons_count = SerializerMethodField()
+
+    def get_lessons_count(self, lesson):
+        return Lesson.objects.filter(course=lesson.course).count()
+
     class Meta:
         model = Lesson
-        fields = '__all__'
+        fields = ("id", "title", "description", "course", "lessons_count")
