4990d37165b416af62abf9f70f203970f15dff86
Danny-progg <122675148+Danny-progg@users.noreply.github.com>
2023-11-08 18:17:26 +0300

    added a field for displaying the number of lessons

--- study/models.py
+++ study/models.py
@@ -3,22 +3,9 @@ from django.db import models
 NULLABLE = {'blank': True, 'null': True}
 
 
-class Course(models.Model):
-    title = models.CharField(max_length=150, verbose_name='название')
-    description = models.TextField(verbose_name='описание')
-    preview = models.ImageField(upload_to='images/', verbose_name='изображение', **NULLABLE)
-
-    def __str__(self):
-        return f'{self.title}'
-
-    class Meta:
-        verbose_name = 'курс'
-        verbose_name_plural = 'курсы'
-
-
 class Lesson(models.Model):
-    title = models.CharField(max_length=150, verbose_name='название')
-    description = models.TextField(verbose_name='описание')
+    title = models.CharField(max_length=150, verbose_name='название', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
     preview = models.ImageField(upload_to='images/', verbose_name='изображение', **NULLABLE)
     link_video = models.CharField(max_length=150, verbose_name='ссылка на видео', **NULLABLE)
 
@@ -28,3 +15,17 @@ class Lesson(models.Model):
     class Meta:
         verbose_name = 'урок'
         verbose_name_plural = 'уроки'
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название', **NULLABLE)
+    description = models.TextField(verbose_name='описание', **NULLABLE)
+    preview = models.ImageField(upload_to='images/', verbose_name='изображение', **NULLABLE)
+    lesson_number = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name='количество уроков', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
